621b71b8fd8ebad588564c660eb29035b5abe95d
hakuunabatata <emachadomartins1999@gmail.com>
2024-01-05 16:27:38 -0300

    add TouchableOpacity to Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,24 @@
-import { Text, View, TextInput } from 'react-native'
+import { Text, View, TextInput, TouchableOpacity } from 'react-native'
 import styles from './styles'
 
-export const Home = () => (
-  <View style={styles.container}>
-    <Text style={styles.eventName}>Nome do Evento</Text>
-    <Text style={styles.eventDate}>Sexta, 05 de Janeiro de 2023</Text>
-    <TextInput
-      style={styles.input}
-      placeholder="Nome do participante"
-      placeholderTextColor="#6B6B6B"
-    />
-  </View>
-)
+export const Home = () => {
+  const handleParticipantAdd = () => {
+    console.log('Clicou no +')
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do Evento</Text>
+      <Text style={styles.eventDate}>Sexta, 05 de Janeiro de 2023</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Nome do participante"
+        placeholderTextColor="#6B6B6B"
+      />
+
+      <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+        <Text style={styles.buttonText}>+</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
