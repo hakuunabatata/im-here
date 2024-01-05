55cc0975df61f78b6b02950dab44ddf048c044d6
hakuunabatata <emachadomartins1999@gmail.com>
2024-01-05 16:17:32 -0300

    add TextInput to Home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,14 @@
-import { Text, View } from 'react-native'
+import { Text, View, TextInput } from 'react-native'
 import styles from './styles'
 
 export const Home = () => (
   <View style={styles.container}>
     <Text style={styles.eventName}>Nome do Evento</Text>
     <Text style={styles.eventDate}>Sexta, 05 de Janeiro de 2023</Text>
+    <TextInput
+      style={styles.input}
+      placeholder="Nome do participante"
+      placeholderTextColor="#6B6B6B"
+    />
   </View>
 )
